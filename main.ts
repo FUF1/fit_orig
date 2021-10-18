6293158eb3d89a8cf570a428ab8161bab4b0aaf0
FUF1 <69962736+FUF1@users.noreply.github.com>
2021-10-19 00:11:17 +0200

    IMU calibration works a bit better. Still issues with only one count per draw.

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 input.onButtonPressed(Button.A, function () {
+    firstTimeFlag = 0
     basic.pause(10)
     while (count <= repetitions) {
-        accel_val = input.acceleration(Dimension.Strength)
-        if (Math.abs(accel_val) >= target_val - tolerance && Math.abs(accel_val) <= target_val - tolerance) {
-            if (accel_val < 0 && prev_val > 0 || accel_val > 0 && prev_val < 0) {
-                prev_val = accel_val
-                count += 1
-                light2 += Math.trunc(255 / repetitions) * -1
-            }
+        if (firstTimeFlag == 0) {
+            basic.pause(2000)
+        }
+        firstTimeFlag = 1
+        basic.pause(100)
+        accel_val = input.acceleration(Dimension.Z)
+        if (accel_val < zero_cal - tolerance / 2 && prev_val > zero_cal + tolerance / 2 || accel_val > zero_cal + tolerance / 2 && prev_val < zero_cal - tolerance / 2) {
+            prev_val = accel_val
+            count += 1
+            light2 += Math.trunc(255 / repetitions) * -1
         }
         basic.setLedColor(basic.rgbw(
         light2,
@@ -16,7 +20,6 @@ input.onButtonPressed(Button.A, function () {
         light2
         ))
         basic.showNumber(count)
-        basic.pause(10)
     }
     if (count == repetitions) {
         music.playMelody("E F G B G - B - ", 350)
@@ -27,8 +30,9 @@ input.onButtonPressed(Button.B, function () {
     count = 0
     basic.pause(100)
 })
+let firstTimeFlag = 0
+let zero_cal = 0
 let tolerance = 0
-let target_val = 0
 let prev_val = 0
 let accel_val = 0
 let light2 = 0
@@ -36,12 +40,16 @@ let repetitions = 0
 let count = 0
 music.playMelody("G B G B B - - - ", 350)
 count = 0
+// Modify
 repetitions = 15
 light2 = 255 + Math.trunc(255 / repetitions)
 accel_val = 0
-prev_val = 1024
-target_val = 1024
-tolerance = 50
+prev_val = 0
+// Modify
+let target_val = 1035
+// Modify
+tolerance = 5
+zero_cal = -1024
 basic.forever(function () {
 	
 })
